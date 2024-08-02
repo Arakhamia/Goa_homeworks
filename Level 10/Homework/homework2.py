347a82ed0daf274d16771c2bbcf608556c950936
Andria <andriaarakhamia@gmail.com>
2024-08-03 00:19:19 +0400

    tenth commit

---
+++ Level 10/Homework/homework2.py
@@ -0,0 +1,5 @@
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+
+print("My name is" + " " + name + "," + "lastname is" + " " + lastname + " " + "and I am" + " " + age + " " + "years old." )
---
+++ Level 10/Homework/homework3.py
@@ -0,0 +1,4 @@
+age = int(input("enter your age: "))
+future_age = age+36
+
+print("Your age after 36 years, will be" + " " + str(future_age))
