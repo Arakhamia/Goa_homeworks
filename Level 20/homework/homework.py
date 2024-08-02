247340afa20ef22aad949be421406a22c27d6988
Andria <andriaarakhamia@gmail.com>
2024-08-03 00:27:24 +0400

    twentieth commit

---
+++ Level 20/homework/homework.py
@@ -0,0 +1,7 @@
+num = int(input("Guess the number: "))
+if num == 18:
+    print("You won toy dinosaur!")
+elif num == 33:
+    print("You won toy rabbit!")
+else:
+    print("You didn't won anything...")
---
+++ Level 20/homework/homework1.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if age < 13:
+    print("You are a kid.")
+elif age > 13 and age < 20:
+    print("You are teenager.")
+else:
+    print("You are grown up.")
